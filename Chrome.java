3924ec628d925d80dc4731711e3c695577006ec4
Guhan28 <117150179+Guhan28@users.noreply.github.com>
2022-11-01 14:12:14 +0530

    Add files via upload
    assignment 3

---
+++ Chrome.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Chrome {
+ public void getname() {
+	 System.out.println("this is my browser");
+ }
+    public void PrintName() {
+	System.out.println("this is google chrome");
+}
+    public static void main(String[] args) {
+		 Chrome obj= new Chrome();
+		 obj.getname();
+		 obj.PrintName();
+		 System.out.println("this is google chrome");
+	}
+}
