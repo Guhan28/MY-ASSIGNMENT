95c4d39753b26e963ffa105e6ec70197aa4e6fed
Guhan28 <117150179+Guhan28@users.noreply.github.com>
2022-11-01 15:49:18 +0530

    Add files via upload
    assignment 5

---
+++ Report.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Report {
+public static void main(String[] args) {
+	Student student=new Student();
+	System.out.println("Student Report");
+	System.out.println("studentname");
+	System.out.println("rollno");
+	System.out.println("collagename");
+	System.out.println("marksscored");
+	System.out.println("cgpa");
+}
+
+}
+
+
---
+++ Student.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class Student {
+String studentname="guhan";
+int rollno=1996;
+String collagename="st.peters";
+short  marksscored=715;
+float cgpa=6.2f;
+}
