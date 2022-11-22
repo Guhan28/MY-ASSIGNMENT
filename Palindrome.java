399345768d9aa3363ab05a6d381bc78cbe7b8ab3
Guhan28 <117150179+Guhan28@users.noreply.github.com>
2022-11-22 11:49:16 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Palindrome {
+public static void main(String[] args) {
+	int num =34343,sum=0,r;
+	int n;
+	n=num;
+	while(num>0)
+	{	
+		r=num%10;
+		sum=(sum*10)+r;
+		num=num/10;
+		
+	}
+	if (n==sum)
+	{	System.out.println(n+"is a palindrome");
+}
+else
+{ 
+	System.out.println(n+"is not a palindrome");
+}
+}
+}
