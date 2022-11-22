db3879deb5929ee1baca7285c222b12c5fb0b3b4
Guhan28 <117150179+Guhan28@users.noreply.github.com>
2022-11-22 12:03:23 +0530

    Add files via upload

---
+++ Converting_Negative_Number_To_Positive_Pumber.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Converting_Negative_Number_To_Positive_Pumber {
+
+	public static void main(String[] args) {
+		
+int num=-40;
+if(num<0)
+{System.out.println(num+"is a negative number");
+	}
+else
+{System.out.println(num+"is not a negative number");
+}
+num=num*-1;
+System.out.println("-40 negative number is converted to"+num+"positive number");
+}
+	}
