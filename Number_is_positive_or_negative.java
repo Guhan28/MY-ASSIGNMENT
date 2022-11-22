132d9fb407409ca2729b6390f738b507b0e7c3e4
Guhan28 <117150179+Guhan28@users.noreply.github.com>
2022-11-22 12:10:43 +0530

    Add files via upload

---
+++ Number_is_positive_or_negative.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Number_is_positive_or_negative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=35;
+if(num>0)
+	System.out.println(num+"is a positive number");
+else if(num<0)
+	System.out.println(num+"is a negative number");
+else
+	System.out.println("neither positive nor negative");
+	}
+
+}
